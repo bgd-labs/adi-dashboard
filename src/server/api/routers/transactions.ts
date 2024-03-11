e8c203929e2a5e6c6198ae3e129d57b7853424e3
K.K. <m@mnm.mn>
2024-03-11 15:14:01 +0200

    feat: add transactions endpoint

---
+++ src/server/api/routers/transactions.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+
+export const transactionsRouter = createTRPCRouter({
+  getTransactionCosts: publicProcedure
+    .input(
+      z.object({
+        txHashes: z.array(z.string()),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const { txHashes } = input;
+      const { data } = await ctx.supabaseAdmin
+        .from("TransactionCosts")
+        .select("*")
+        .in("transaction_hash", txHashes);
+
+      return data ?? [];
+    }),
+  getGasCosts: publicProcedure
+    .input(
+      z.object({
+        txHashes: z.array(z.string()),
+      }),
+    )
+    .query(async ({ input, ctx }) => {
+      const { txHashes } = input;
+      const { data } = await ctx.supabaseAdmin
+        .from("TransactionGasCosts")
+        .select("*")
+        .in("transaction_hash", txHashes);
+
+      return data ?? [];
+    }),
+});
