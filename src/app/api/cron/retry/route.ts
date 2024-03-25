d93c6827991ac3dad9289a65117e60cd32fa5169
K.K. <m@mnm.mn>
2024-03-25 13:09:23 +0200

    chore: add maxDuration

--- src/app/api/cron/retry/route.ts
+++ src/app/api/cron/retry/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { env } from "@/env";
 import { retryEvents } from "@/server/eventCollection";
 
+export const maxDuration = 200;
+
 export const GET = async (req: Request) => {
   if (req.headers.get("Authorization") !== `Bearer ${env.CRON_SECRET}`) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
